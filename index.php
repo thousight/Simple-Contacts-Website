da3216f79e34b745df7b10bb8641e54fbab08024
Mark Wen <markwenguojie94@gmail.com>
2015-06-18 17:15:40 -0700

    Simple Contacts Website
    Learn and used WAMP

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+    <?php
+        session_start();
+    ?>
+    
+<form action="login.php" method="post">
+username: <input type="text" name="username"><br>
+password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ update.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+
+<form method="post">
+Name: <input type="text" name="Name"><br>
+Phone Number: <input type="text" name="Number"><br>
+<input type="submit">
+</form>
+    
+<?php 
+    session_start();
+// Handling when the form isn't filled or isn't filled completely
+if (empty($_POST["Name"]) || empty($_POST["Number"])){
+    echo "Please enter name and phone number";
+}
+
+// When the form is filled, post all the variables and store them.
+else {   
+    $name = $_POST["Name"];
+    $number = $_POST["Number"];
+    $sql = "UPDATE contacts SET Numbers='" . $number . "' WHERE Name='" . $name . "'";// Creating SQL add statement
+    $connect = new mysqli("localhost", $_SESSION["username"],         $_SESSION["password"], "schema"); // Using session to create a new connection
+    
+// Add data to mysql in the if statement and show results
+if ($connect->query($sql) === true) {
+    echo "Success";
+}
+else {
+    echo "Fail: " . $connect->error;
+}
+}
+?>
+
+</body>
+</html>
